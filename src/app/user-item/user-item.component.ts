44c322d960fa317bde5cadbe60ced0776528538b
Jesus <jesus@ensolvers.com>
2019-12-10 13:57:25 -0300

    Initial steps on Angular

---
+++ src/app/user-item/user-item.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.css']
+})
+export class UserItemComponent implements OnInit {
+  public name: string;
+
+  constructor() {
+    this.setName('Felipe');
+  }
+
+  ngOnInit() {
+  }
+
+  /**
+   * setName
+   */
+  public setName(name: string): void {
+    this.name = name;
+  }
+
+  /**
+   * getName
+   */
+  public getName(): string {
+    return this.name;
+  }
+}
